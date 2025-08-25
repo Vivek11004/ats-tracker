2352b2a14b08a06a67040f608884c9703a50565d
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-08-25 16:12:42 +0530

    Update app.js

--- fronted/app.js
+++ fronted/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'https://ats-tracker-4.onrender.com';
+    const API_BASE_URL = 'https://ats-tracker-5.onrender.com';
 
     // State
     let resumeData = null;
