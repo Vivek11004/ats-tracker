9daef8ee04689302e7ee5fc4a026d160da9bf19c
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-08-30 19:44:05 +0530

    Update app.js

--- fronted/app.js
+++ fronted/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'ats-tracker-nine.vercel.app';
+    const API_BASE_URL = 'https://ats-tracker-nine.vercel.app';
 
     // State
     let resumeData = null;
