10ef34d0d900801e61c60c4ac518b144b7302ecd
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-11-09 23:46:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -86,4 +86,4 @@ if __name__ == "__main__":
         nltk.download('stopwords')
         
     PORT = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=PORT)
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
