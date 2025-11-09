7dbbeeea5f3bc723cbf023918de07b0011beff61
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-11-09 22:26:39 +0530

    Update app.js

--- fronted/app.js
+++ fronted/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'https://energetic-learning-production.up.railway.app/';
+    const API_BASE_URL = 'https://ats-tracker-production.up.railway.app/';
 
     // State
     let resumeData = null;
