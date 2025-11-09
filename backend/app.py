d03dae7f75c7beab1764687f88f1f1407568b6ef
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-11-10 01:15:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -112,4 +112,4 @@ if __name__ == "__main__":
 
     # Dynamic port for Railway/Render
     PORT = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=Port)
